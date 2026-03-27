51f2369345e9d650591ec963b5a53f5db74ec542
ricardoo-azevedo <ricardoazevedo1911@gmail.com>
2026-03-26 21:18:15 -0300

    fix: configurando prisma

---
+++ prisma.config.js
@@ -0,0 +1,14 @@
+
+require("dotenv").config();
+import { defineConfig, env } from "prisma/config";
+
+export default defineConfig({
+  schema: "src/prisma/schema.prisma", 
+  migrations: {
+    path: "src/prisma/migrations", 
+  },
+  datasource: {
+    adapter: "mysql",
+    url: env("DATABASE_URL"),
+  },
+});
