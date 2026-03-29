1f80e5df1ac33e5d93ef43f1bb619f5de4eb80d6
sfrancileidy-prog <sfrancileidy@gmail.com>
2026-03-29 15:56:21 -0300

    feat: adicionando atendimento routes

--- src/app.js
+++ src/app.js
@@ -5,9 +5,11 @@ app.use(express.json());
 
 const guicheRoutes = require('./routes/guicheRoutes');
 const ticketRoutes = require('./routes/ticketRoutes');
+const atendimentoRoutes = require('./routes/atendimentoRoutes');
 
 app.use('/guiches', guicheRoutes);
 app.use('/tickets', ticketRoutes);
+app.use('/atendimentos', atendimentoRoutes);
 
 const PORT = 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
