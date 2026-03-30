ce3dc4395c0deaf4a604cb85d1525ed2b1cdfd2a
ricardoo-azevedo <ricardoazevedo1911@gmail.com>
2026-03-30 13:32:16 -0300

    delete: removendo projeto do diretorio errado

--- src/prisma/migrations/20260326234946_init/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE `Ticket` (
-    `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `codigo` VARCHAR(191) NOT NULL,
-    `dataEmissao` DATETIME(3) NOT NULL,
-    `dataChamada` DATETIME(3) NULL,
-
-    PRIMARY KEY (`id`)
-) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
--- src/prisma/migrations/20260327102246_init/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `guicheId` on the `Atendimento` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE `Atendimento` DROP COLUMN `guicheId`;
